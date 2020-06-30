0288d83efeee4c3e433f306c68f0aa6c16a0cb2c
alanblee <alanbenlee12@gmail.com>
2020-06-30 15:42:33 -0700

    structured out the header/nav

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Header from "./header/header";
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
