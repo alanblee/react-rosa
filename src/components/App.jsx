adaf68432c6ef5afd3d8082782f27fc290882666
alanblee <alanbenlee12@gmail.com>
2020-06-30 17:55:23 -0700

    added hero section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./header/header";
+import HeroSection from "./hero-section/hero-section";
 function App() {
   return (
     <div className="">
       <Header />
+      <HeroSection />
     </div>
   );
 }
