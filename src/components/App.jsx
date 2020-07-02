2bd3efa98785ab0d46052f3e84aa8b003b47f711
alanblee <alanbenlee12@gmail.com>
2020-07-02 14:38:35 -0700

    discover info markup  Please enter the commit message for your changes. Lines starting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./header/header";
 import HeroSection from "./hero-section/hero-section";
+import DiscoverSection from "./discover-section/discover-section";
 function App() {
   return (
     <div className="">
       <Header />
       <HeroSection />
+      <DiscoverSection />
     </div>
   );
 }
